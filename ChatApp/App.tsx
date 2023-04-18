dd5a5884fb6ef33643abdc26bea2ac597f4ee8d5
delaying <pgy5638@naver.com>
2023-04-18 16:06:43 +0900

    홈스크린 구현완료

--- ChatApp/App.tsx
+++ ChatApp/App.tsx
@@ -10,21 +10,44 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
 
 import { RootStackParamList } from './src/types';
 import SignupScreen from './src/SignupScreen/SignupScreen';
 import AuthProvider from './src/components/AuthProvider';
 import SigninScreen from './src/SigninScreen/SigninScreen';
+import AuthContext from './src/components/AuthContext';
+import HomeScreen from './src/HomeScreen/HomeScreen';
+import LoadingScreen from './src/LoadingScreen/LoadingScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Screens = () => {
+  const { user, processingSignin, processingSignup, initialized } =
+    useContext(AuthContext);
+
+  // 로그인상태에 따라서 홈화면이나 로그인화면 보여주기
+  const renderRootStack = useCallback(() => {
+    if (!initialized) {
+      return <Stack.Screen name="Loading" component={LoadingScreen} />;
+    }
+    if (user != null && !processingSignin && !processingSignup) {
+      //login
+      return <Stack.Screen name="Home" component={HomeScreen} />;
+    }
+    //logout
+    return (
+      <>
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Signin" component={SigninScreen} />
+      </>
+    );
+  }, [user, processingSignin, processingSignup, initialized]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Signin" component={SigninScreen} />
+        {renderRootStack()}
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ ChatApp/src/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,181 @@
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+import Screen from '../components/Screen';
+import AuthContext from '../components/AuthContext';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Colors from '../modules/Colors';
+import { Collections, User } from '../types';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  sectionTitleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: Colors.BLACK,
+  },
+  userSectionContent: {
+    backgroundColor: Colors.BLACK,
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  myProfile: {
+    flex: 1,
+  },
+  myNameText: {
+    color: Colors.WHITE,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  myEmailText: {
+    marginTop: 4,
+    color: Colors.WHITE,
+    fontSize: 14,
+  },
+  logoutText: {
+    color: Colors.WHITE,
+    fontSize: 14,
+  },
+  userListSection: {
+    flex: 1,
+    marginTop: 40,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  userList: {
+    flex: 1,
+  },
+  userListItem: {
+    backgroundColor: Colors.LIGHT_GRAY,
+    borderRadius: 12,
+    padding: 20,
+  },
+  otherNameText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: Colors.BLACK,
+  },
+  otherEmailText: {
+    marginTop: 4,
+    fontSize: 14,
+    color: Colors.BLACK,
+  },
+  separator: {
+    height: 10,
+  },
+  emptyText: {
+    color: Colors.BLACK,
+  },
+});
+
+const HomeScreen = () => {
+  const { user: me } = useContext(AuthContext);
+  const [loadingUsers, setLoadingUsers] = useState(false);
+  const [users, setUsers] = useState<User[]>([]);
+
+  //   로그아웃
+  const onPressLogout = useCallback(() => {
+    auth().signOut();
+  }, []);
+
+  const loadUsers = useCallback(async () => {
+    try {
+      setLoadingUsers(true);
+      const snapshot = await firestore().collection(Collections.USERS).get();
+      // 자신을 제외한 저장된 사람들을 map함수로 불러주기
+      setUsers(
+        snapshot.docs
+          .map(doc => doc.data() as User)
+          .filter(u => u.userId !== me?.userId),
+      );
+    } finally {
+      setLoadingUsers(false);
+    }
+  }, [me?.userId]);
+
+  useEffect(() => {
+    loadUsers();
+  }, [loadUsers]);
+
+  const renderLoading = useCallback(
+    () => (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator />
+      </View>
+    ),
+    [],
+  );
+
+  if (me == null) {
+    return null;
+  }
+
+  return (
+    <Screen title="홈">
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.sectionTitleText}>나의 정보</Text>
+          <View style={styles.userSectionContent}>
+            <View style={styles.myProfile}>
+              <Text style={styles.myNameText}>{me?.name}</Text>
+              <Text style={styles.myEmailText}>{me?.email}</Text>
+            </View>
+            <TouchableOpacity onPress={onPressLogout}>
+              <Text style={styles.logoutText}>로그아웃</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.userListSection}>
+          {loadingUsers ? (
+            renderLoading()
+          ) : (
+            <>
+              <Text style={styles.sectionTitleText}>
+                다른 사용자와 대화해보세요!
+              </Text>
+              <FlatList
+                style={styles.userList}
+                data={users}
+                renderItem={({ item: user }) => (
+                  <TouchableOpacity
+                    style={styles.userListItem}
+                    onPress={() => {}}>
+                    <Text style={styles.otherNameText}>{user.name}</Text>
+                    <Text style={styles.otherEmailText}>{user.email}</Text>
+                  </TouchableOpacity>
+                )}
+                // 아이템 간의 간격조정
+                ItemSeparatorComponent={() => <View style={styles.separator} />}
+                //배열이 비어있을때 보여주는 화면
+                ListEmptyComponent={() => {
+                  return (
+                    <Text style={styles.emptyText}>사용자가 없습니다.</Text>
+                  );
+                }}
+              />
+            </>
+          )}
+        </View>
+      </View>
+    </Screen>
+  );
+};
+
+export default HomeScreen;
