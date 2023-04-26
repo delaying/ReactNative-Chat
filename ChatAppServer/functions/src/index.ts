b2bafb9b8fa76d98f1a3a81134818c58e27769b9
delaying <pgy5638@naver.com>
2023-04-26 22:11:12 +0900

    ChatAppServer init

---
+++ ChatAppServer/functions/src/index.ts
@@ -0,0 +1,10 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// 다음 내용은 https를 이용한 function임. helloworld 주소로 실행시킬 수 있다는 코드
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
